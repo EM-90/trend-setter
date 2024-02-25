5a06b6d669c1bf66ae3cf40721d484797b1e0182
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-02-26 00:24:08 +0100

    Added styling to the productCards, wraped outlet inside container, styled navbar

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -1,12 +1,15 @@
 import { Outlet } from "react-router-dom";
-import { Navbar } from "./navbar/Navbar";
+import { Navbar } from "./navbar/Index";
 import { Footer } from "./footer/Footer";
+import styles from './contentContainer.module.css'
 
 export default function Layout() {
     return (
        <div>
         <Navbar/>
-        <Outlet/>
+        <div className={styles.contentContainer}>
+          <Outlet/>
+        </div>
         <Footer/>
       </div>
     )
--- src/components/layout/navbar/Index.jsx
+++ src/components/layout/navbar/Index.jsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom"
+import styles from './navbar.module.css'
 
 export const Navbar = () => {
-    return( <div>
+    return( <div className={styles.navBar}>
                <div className="Logo">
                         TrendSetter
                     </div>
-                <ul className="links">
+                <ul className={styles.links}>
                     <li><Link to="/">Home</Link></li>
-                    <li><Link to="/cart">Cart</Link></li>
                     <li><Link to="/contact">Contact</Link></li>
+                    <li><Link to="/cart">Cart</Link></li>
                 </ul>
             </div>
         
