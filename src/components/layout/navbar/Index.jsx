e485c84567afaf3edf86d5bbe00cb3c21a852dd0
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-03-04 14:42:29 +0100

    Added cart icon

--- src/components/layout/navbar/Index.jsx
+++ src/components/layout/navbar/Index.jsx
@@ -1,5 +1,7 @@
 import { Link } from "react-router-dom"
 import styles from './navbar.module.css'
+import { RiShoppingCartLine } from "react-icons/ri";
+
 
 export const Navbar = () => {
     return( <div className={styles.navBar}>
@@ -10,7 +12,7 @@ export const Navbar = () => {
                     <ul className={styles.links}>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/contact">Contact</Link></li>
-                        <li><Link to="/cart">Cart</Link></li>
+                        <li><Link to="/cart"><RiShoppingCartLine size={32}/></Link></li>
                     </ul>
                 </div>
             </div>
