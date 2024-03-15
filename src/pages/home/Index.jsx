1c93b92fc643bda5d6a1675596aa57bde4f8884d
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-03-15 12:12:43 +0100

    Added the search function to the Home page

--- src/pages/home/Index.jsx
+++ src/pages/home/Index.jsx
@@ -1,15 +1,24 @@
 import Search from "../../components/common/input-fields/search/Index"
+import { useState } from "react"
+import { RenderProductCard } from "../../components/rendering/RenderProductCard"
 import styles from "./home.module.css"
 
 
 export const Home = () => {  
   const apiUrl = "https://v2.api.noroff.dev/online-shop"
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSearch = (term) => {
+    setSearchTerm(term);
+  }
 
   return (
-    <div className={styles.flexContainer}>
-      <Search apiUrl={apiUrl}/>
+    <div className={styles.container}>
+      <Search onSearch={handleSearch} />
+      <div className={styles.flexContainer}>
+        <RenderProductCard apiUrl={apiUrl} searchTerm={searchTerm} />
+      </div>
     </div>
-  )
-}
-
+  );
+};
 
