e83b5205a083d0eb994d6f85a80a9582cf0ac697
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-03-18 11:56:49 +0100

    Added the rest of the html for the product details page

--- src/pages/productDetails/Productdetails.jsx
+++ src/pages/productDetails/Productdetails.jsx
@@ -22,9 +22,24 @@ export default function ProductDetails() {
             {product && (
                 <>
                     <h1>Product details</h1>
-                    <div>
-                        {/* Check if product.image exists before accessing its url property */}
+                    <div className="productDetailsContainer">
                         {product.image && <img src={product.image.url} alt={product.title} />}
+                        <div>
+                            <h3>{product.title}</h3>
+                            <div className="priceContainer">
+                                <p>{product.price}</p>
+                                <p>{product.discountedPrice}</p>
+                            </div>
+                            <p>Rating:{product.rating}/5</p>
+                            <div className="descContainer">
+                                <h4>Description</h4>
+                                <p>{product.description}</p>
+                            </div>
+                            <div className="buttonContainer">
+                                <button className="secondary">Wishlist</button>
+                                <button className="primary">Add to cart</button>
+                            </div>
+                        </div>
                     </div>
                 </>
             )}
