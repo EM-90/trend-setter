ddc5c1a5871b3745804bfa38f64775ac7c45c93b
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-03-28 14:57:33 +0100

    removed import not in use

--- src/pages/cart/Index.jsx
+++ src/pages/cart/Index.jsx
@@ -1,57 +1,59 @@
+/** @format */
+
 import { useCart } from "../../context/cartContext/CartContext";
-import calculateProduct from "../../components/product/calculateProduct/FindPrecentage";
 import styles from "./itemInCart.module.css";
 import findPrecentage from "../../components/product/calculateProduct/FindPrecentage";
 
 export const Cart = () => {
-    const { cartItems, decrementQuantity, incrementQuantity, removeFromCart } = useCart();
+  const { cartItems, decrementQuantity, incrementQuantity, removeFromCart } =
+    useCart();
 
-    const handleDecreaseQuantity = (index) => {
-      decrementQuantity(index);
+  const handleDecreaseQuantity = (index) => {
+    decrementQuantity(index);
   };
-  
-    const handleIncreaseQuantity = (index) => {
-      incrementQuantity(index);
-    };
 
-    const handelRemoveFromCart = () => {
-      removeFromCart();
-    }
-   
+  const handleIncreaseQuantity = (index) => {
+    incrementQuantity(index);
+  };
 
-    return (
-      <div>
-        <h2>Cart</h2>
-        {cartItems.map((item, index) => (
-          <div className={styles.cartProduct} key={index}>
-            <div className={styles.imageContainer}>
-             <img className={styles.productImage} src={item.image.url} alt={item.title} />
-            </div>
-            <div className={styles.productContent}>
+  const handelRemoveFromCart = () => {
+    removeFromCart();
+  };
+
+  return (
+    <div>
+      <h2>Cart</h2>
+      {cartItems.map((item, index) => (
+        <div className={styles.cartProduct} key={index}>
+          <div className={styles.imageContainer}>
+            <img
+              className={styles.productImage}
+              src={item.image.url}
+              alt={item.title}
+            />
+          </div>
+          <div className={styles.productContent}>
             <h3>{item.title}</h3>
             <div className="priceSection">
               <p className="price">{item.price}</p>
               <p className="discountedPrice">{item.discountedPrice}</p>
-              <p className="productDiscount">{findPrecentage(item.price, item.discountedPrice)}% off</p>
+              <p className="productDiscount">
+                {findPrecentage(item.price, item.discountedPrice)}% off
+              </p>
             </div>
             <span className="rating">Rating {item.rating}/5</span>
             <p className="productDescription">{item.description}</p>
-            </div>
-            <div className={styles.addProductContainer}>
-              <button onClick={() => handleDecreaseQuantity(index)}>-</button>
-              <input type="number" value={item.quantity} readOnly />
-              <button onClick={() => handleIncreaseQuantity(index)}>+</button>
-            </div>
-            <button onClick={() => handelRemoveFromCart()}>Remove product</button>
           </div>
-        ))}
-      </div>
-    ); 
-}
+          <div className={styles.addProductContainer}>
+            <button onClick={() => handleDecreaseQuantity(index)}>-</button>
+            <input type="number" value={item.quantity} readOnly />
+            <button onClick={() => handleIncreaseQuantity(index)}>+</button>
+          </div>
+          <button onClick={() => handelRemoveFromCart()}>Remove product</button>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default Cart;
-
-
-
-
-
