f5ace5bbd832b87cfbc2dcc7f8e49fae27a1908c
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-03-28 14:48:09 +0100

    fixed handelRemoveFromCart function by passing the index

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+/** @format */
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import { createRoot } from "react-dom";
+import App from "./App.jsx";
+import "./index.css";
+
+createRoot(document.getElementById("root")).render(
   //<React.StrictMode>
-    <App />
-  //</React.StrictMode>,
-)
+  <App />
+  //</React.StrictMode>
+);
