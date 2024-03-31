ce8bcce3ebc6c373823caa86faa4de5c9d6ef941
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-03-31 21:02:18 +0200

    Added some styling to the product details page

--- src/pages/productDetails/Productdetails.jsx
+++ src/pages/productDetails/Productdetails.jsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import PrimaryButton from "../../components/common/buttons/primaryButton/Index";
 import findPrecentage from "../../components/product/calculateProduct/FindPrecentage";
+import styles from "./productDetails.module.css";
 
 export default function ProductDetails() {
   const { id } = useParams();
@@ -31,16 +32,26 @@ export default function ProductDetails() {
       {product && (
         <>
           <h1>Product details</h1>
-          <div className="productDetailsContainer">
-            {product.image && (
-              <img src={product.image.url} alt={product.title} />
-            )}
-            {precentage > 0 && <p>{precentage}% off</p>}
-            <div>
+          <div className={styles.productDetailsContainer}>
+            <div className={styles.imageContainer}>
+              {product.image && (
+                <img
+                  className={styles.productImage}
+                  src={product.image.url}
+                  alt={product.title}
+                />
+              )}
+              {precentage > 0 && (
+                <p className={styles.discountBadge}>{precentage}% off</p>
+              )}
+            </div>
+            <div className={styles.productTextContainer}>
               <h3>{product.title}</h3>
-              <div className="priceContainer">
-                <p>{product.price}</p>
-                <p>{product.discountedPrice}</p>
+              <div className={styles.priceContainer}>
+                <p className={styles.price}>${product.price}</p>
+                <p className={styles.discountPrice}>
+                  ${product.discountedPrice}
+                </p>
               </div>
               <p>Rating:{product.rating}/5</p>
               <div className="descContainer">
