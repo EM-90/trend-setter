60352cf48596db4f3aa976ab78b873b64d8df77e
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-04-04 18:55:54 +0200

    small changes

--- src/components/product/productCard/Index.jsx
+++ src/components/product/productCard/Index.jsx
@@ -2,19 +2,15 @@
 
 import { Link } from "react-router-dom";
 import styles from "./productCard.module.css";
-import findPrecentage from "../calculateProduct/FindPrecentage";
+import DiscountBadge from "../../common/discountBadge/Index";
 import Rating from "../../common/rating/Index";
 
 export default function ProductCard(props) {
-  const discountPercentage = findPrecentage(props.price, props.discountedPrice);
-
   return (
     <Link to={`/product/${props.id}`} className={styles.card}>
       <div className="imageContainer">
         <img src={props.image} alt={props.title} className={styles.cardImage} />
-        {discountPercentage > 0 && (
-          <p className={styles.discountBadge}>-{discountPercentage}%</p>
-        )}
+        <DiscountBadge product={props} />
       </div>
       <div className={styles.cardContent}>
         <h3 className={styles.cardTitle}>{props.title}</h3>
