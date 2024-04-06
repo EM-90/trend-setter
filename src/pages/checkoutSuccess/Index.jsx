580fe9b01c7853887972588213cbcf050e868e42
EM-90 <94170154+EM-90@users.noreply.github.com>
2024-04-06 12:02:57 +0200

    Added link back to home page on checkout success page

--- src/pages/checkoutSuccess/Index.jsx
+++ src/pages/checkoutSuccess/Index.jsx
@@ -1,10 +1,21 @@
 /** @format */
+import { Link } from "react-router-dom";
+import styles from "./checkoutSuccess.module.css";
+import { RiArrowDropLeftLine } from "react-icons/ri";
 
 function CheckoutSuccess() {
   return (
     <>
-      <h2>Thank you for your purchase</h2>
-      <h4>You will recive a receipt on e-mail</h4>
+      <div className={styles.successMessage}>
+        <h2>Thank you for your purchase</h2>
+        <h4>You will recive a receipt on e-mail</h4>
+      </div>
+      <div className={styles.linkContainer}>
+        <Link className={styles.link} to="/">
+          <RiArrowDropLeftLine size={32} />
+          Go back to main page
+        </Link>
+      </div>
     </>
   );
 }
